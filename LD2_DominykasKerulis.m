d03003594e78d10099c5652125462136271ab20f
Night Nation <dominykaskerulis@gmail.com>
2026-09-18 10:42:35 +0300

    Update LD2_DominykasKerulis.m

--- LD2_DominykasKerulis.m
+++ LD2_DominykasKerulis.m
@@ -20,6 +20,36 @@ sum(C, 2)
 
 
 
+A = 5.5;           
+f = 8;              
+sigma = 0.8;        
+U1 = 3.5;           
+U2 = 1.5;          
+
+
+t = 0 : 0.002 : 1.2;
+
+
+s_svarus = A * sin(2 * pi * f * t);
+n = sigma * randn(size(t));
+s = s_svarus + n;    
+
+s_atrinktas = s(s > U1);
+
+s_filtruotas = s;
+s_filtruotas(abs(s_filtruotas) < U2) = 0;
+
+
+dydis_s = length(s)
+
+dydis_s_atrinktas = length(s_atrinktas)
+
+
+max_filtruotas = max(s_filtruotas)
+min_filtruotas = min(s_filtruotas)
+
+
+
 
 
 
